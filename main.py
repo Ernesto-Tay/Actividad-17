328f7f2ab8512dc574ac803a88634863073e1745
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-08-09 11:56:43 -0600

    Lista de productos

---
+++ main.py
@@ -0,0 +1,4 @@
+productos = [{"Nombre": "Cartón de huevos","Tipo": "Comida" , "Precio": 35},{"Nombre": "Queso","Tipo": "Comida" , "Precio": 20},{"Nombre": "Macarrones","Tipo": "Comida" , "Precio": 10},{"Nombre": "Shampoo","Tipo": "Higiene" , "Precio": 60},{"Nombre": "Jabón","Tipo": "Higiene" , "Precio": 5},{"Nombre": "Detergente","Tipo": "Limpieza" , "Precio": 30},{"Nombre": "Cloro","Tipo": "Limpieza" , "Precio": 40},{"Nombre": "Secadora de pelo","Tipo": "Belleza" , "Precio": 150},{"Nombre": "Pintauñas","Tipo": "Belleza" , "Precio": 70},{"Nombre": "Camisa","Tipo": "Ropa" , "Precio": 70 },{"Nombre": "Pantalón","Tipo": "Ropa" , "Precio": 125}]
+
+
+
